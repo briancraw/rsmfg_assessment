562af86f43417c0444a79e2145d462bccdb8e052
Brian Craw <craw.brian@gmail.com>
2019-01-23 21:34:28 -0500

    Added packager and Windows installer scripts

---
+++ electron/rsmfg_project/installers/windows/createinstaller.js
@@ -0,0 +1,25 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig () {
+  console.log('creating windows installer')
+  const rootPath = path.join('./')
+  const outPath = path.join(rootPath, 'rsmfg-assessment-app-win32-x64')
+
+  return Promise.resolve({
+    appDirectory: outPath,//path.join(outPath, 'rsmfg-assessment-app-win32-x64/'),
+    authors: 'Brian Craw',
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'windows-installer'),
+    exe: 'rsmfg-assessment-app.exe',
+    setupExe: 'RsmfgAssessmentAppInstaller.exe',
+    setupIcon: path.join(rootPath, 'app', 'img', 'icon_Hpx_icon.ico')
+  })
+}
